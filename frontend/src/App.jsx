c68e934d2a3e63ce0ff3a271200f75c003a6ca61
renil <renilgarala360004@gmail.com>
2025-01-03 23:46:44 +0530

    setup front-end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+        <Navbar/>
+        <Home/>
+        <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      footer fnc ldnjc
+    </div>
+  )
+}
+
+export default Footer
